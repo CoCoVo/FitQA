ef79375c0bc3394be9cabafe45145e8b15a8ef5e
olion500 <olion500@daum.net>
2022-02-25 14:58:12 +0900

    Add Bottom Navigation

---
+++ lib/screen/screen_setting.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScreenSetting extends StatelessWidget {
+  const ScreenSetting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Setting Screen'),
+      ),
+    );
+  }
+}
