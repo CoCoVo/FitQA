b1ccd2b2c47ec0d0bf49cf63a763513fa7162e5d
olion500 <olion500@daum.net>
2022-04-05 10:12:35 +0900

    Added TextFields

--- fitqa_flutter/lib/src/presentation/screens/screen_feedback_request.dart
+++ fitqa_flutter/lib/src/presentation/screens/screen_feedback_request.dart
@@ -17,44 +17,49 @@ class ScreenFeedbackRequest extends StatelessWidget {
             FeedbackCollapsedAppbar(),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(children: [
-                SizedBox(
-                  height: 24,
-                ),
-                Text(
-                  "안내사항",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                BulletPointText(textStyle: TextStyle(fontSize: 12), children: [
-                  "궁금한 내용이나 영상에 대한 추가설명은 글로 작성해주세요.",
-                  "질문자의 체격 조건을 파악하여 맞춤 솔루션을 드립니다.",
-                  "보내주시는 영상은 가급적 0분 이내로 맞춰 주세요.",
-                  "자세가 잘 보이도록 정확하고 선명한 영상을 전달해주시면 더 좋은 피드백이 가능합니다."
-                ]),
-                SizedBox(
-                  height: 20,
-                ),
-                FTextfield(
-                  labelText: "제목",
-                  hintText: "예시) 벤치할 때 팔꿈치가 아픕니다",
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                FTextfield(
-                  labelText: "내용",
-                  maxLines: 10,
-                  hintText:
-                      "예시) 안녕하세요. 운동 n년차 바디빌딩 헬린이입니다. 중량이 올라가면서 팔꿈치가 많이 아픕니다.",
-                ),
-                SizedBox(
-                  height: 18,
-                ),
-                LabeledCheckbox(label: "비공개로 올리기", onChanged: (val) => {}),
-              ]),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      "안내사항",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    BulletPointText(
+                        textStyle: TextStyle(fontSize: 12),
+                        children: [
+                          "궁금한 내용이나 영상에 대한 추가설명은 글로 작성해주세요.",
+                          "질문자의 체격 조건을 파악하여 맞춤 솔루션을 드립니다.",
+                          "보내주시는 영상은 가급적 0분 이내로 맞춰 주세요.",
+                          "자세가 잘 보이도록 정확하고 선명한 영상을 전달해주시면 더 좋은 피드백이 가능합니다."
+                        ]),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FTextfield(
+                      labelText: "제목",
+                      hintText: "예시) 벤치할 때 팔꿈치가 아픕니다",
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    FTextfield(
+                      labelText: "내용",
+                      maxLines: 10,
+                      hintText:
+                          "예시) 안녕하세요. 운동 n년차 바디빌딩 헬린이입니다. 중량이 올라가면서 팔꿈치가 많이 아픕니다.",
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    LabeledCheckbox(label: "비공개로 올리기", onChanged: (val) => {}),
+                  ]),
             ),
             Expanded(
               child: Align(
