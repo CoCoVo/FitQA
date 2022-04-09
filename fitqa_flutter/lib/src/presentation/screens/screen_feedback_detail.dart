76f8da78e1ce86e5b5d8c2b5cd261419a453c4e1
olion500 <olion500@daum.net>
2022-04-09 18:20:20 +0900

    Chore: Add network_video_player
    Add widget named network_video_player. It requires url and show the video. Multi-Dex on android is required to build video-player library.

--- fitqa_flutter/lib/src/presentation/screens/screen_feedback_detail.dart
+++ fitqa_flutter/lib/src/presentation/screens/screen_feedback_detail.dart
@@ -1,18 +1,15 @@
 import 'package:fitqa/src/presentation/widgets/common/carousel_with_indicator.dart';
 import 'package:fitqa/src/presentation/widgets/common/fitqa_appbar_sub.dart';
+import 'package:fitqa/src/presentation/widgets/common/network_video_player.dart';
 import 'package:fitqa/src/theme/color.dart';
 import 'package:flutter/material.dart';
 
 class ScreenFeedbackDetail extends StatelessWidget {
   ScreenFeedbackDetail({Key? key}) : super(key: key);
 
-  final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  final List<String> videoList = [
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
   ];
 
   @override
@@ -27,13 +24,8 @@ class ScreenFeedbackDetail extends StatelessWidget {
         body: ListView(padding: EdgeInsets.zero, children: [
           CarouselWithIndicator(
             height: 400,
-            children: imgList.map((url) {
-              return Image.network(
-                url,
-                fit: BoxFit.fill,
-                width: MediaQuery.of(context).size.width,
-                height: 400,
-              );
+            children: videoList.map((url) {
+              return NetworkVideoPlayer(url: url);
             }).toList(),
           ),
         ]));
