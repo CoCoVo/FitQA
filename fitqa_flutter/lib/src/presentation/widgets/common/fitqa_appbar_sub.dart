27b7ab41874d1250bbc7eb9cca2d4ced91b24cd3
olion500 <olion500@daum.net>
2022-04-09 17:18:06 +0900

    Fix: Add FitqaAppbarSub onPressed event
    Add onPressed event on the AppbarSub. When back icon clicked, the event will be fired.

--- fitqa_flutter/lib/src/presentation/widgets/common/fitqa_appbar_sub.dart
+++ fitqa_flutter/lib/src/presentation/widgets/common/fitqa_appbar_sub.dart
@@ -3,20 +3,28 @@ import 'package:fitqa/src/theme/dimen.dart';
 import 'package:flutter/material.dart';
 
 class FitqaAppbarSub extends StatelessWidget with PreferredSizeWidget {
-  FitqaAppbarSub({Key? key, required this.title, this.actions})
+  FitqaAppbarSub({Key? key, required this.title, this.actions, this.onPressed})
       : super(key: key);
 
   String title;
   List<Widget>? actions;
+  Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        leading: const Icon(
-          Icons.arrow_back_ios_rounded,
-          color: FColors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_rounded,
+            color: FColors.white,
+          ),
+          onPressed: onPressed,
         ),
-        title: Text(title),
+        title: Text(
+          title,
+          style: const TextStyle(
+              fontSize: 18, color: FColors.white, fontWeight: FontWeight.bold),
+        ),
         titleSpacing: 0,
         elevation: 0,
         backgroundColor: FColors.transparent,
