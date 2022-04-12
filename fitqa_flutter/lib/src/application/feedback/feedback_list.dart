92b946f6996b4c1084ec10bf4492f79bb4f4e095
olion500 <olion500@daum.net>
2022-04-12 11:38:31 +0900

    Feat: Impl feedback application layer
    Add feedback list/detail provider in application layer.

--- fitqa_flutter/lib/src/application/feedback/feedback_list.dart
+++ fitqa_flutter/lib/src/application/feedback/feedback_list.dart
@@ -4,23 +4,23 @@ import 'package:fitqa/src/domain/services/feedback/feedback_service.dart';
 import 'package:fitqa/src/domain/services/feedback/feedback_service_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// selected feedback token to show details.
-final selectedFeedbackDetailToken = StateProvider<String>((ref) => "");
-
-final feedbackDetailProvider =
-    StateNotifierProvider<FeedbackDetailNotifier, State<Feedback>>((ref) {
+final feedbackListProvider =
+    StateNotifierProvider<FeedbackListNotifier, State<List<Feedback>>>((ref) {
   final feedbackService = ref.watch(feedbackServiceProvider);
-  return FeedbackDetailNotifier(feedbackService);
+  return FeedbackListNotifier(feedbackService);
 });
 
-class FeedbackDetailNotifier extends StateNotifier<State<Feedback>> {
-  FeedbackDetailNotifier(this.feedbackService) : super(const State.init());
+class FeedbackListNotifier extends StateNotifier<State<List<Feedback>>> {
+  FeedbackListNotifier(this.feedbackService) : super(const State.init()) {
+    getFeedbackList();
+  }
+
   FeedbackService feedbackService;
 
-  void getFeedback(String feedbackToken) async {
+  void getFeedbackList() async {
     try {
       state = const State.loading();
-      final feedback = await feedbackService.getFeedbackDetail(feedbackToken);
+      final feedback = await feedbackService.getFeedbackAll();
       state = State.success(feedback);
     } on Exception catch (e) {
       state = State.error(e);
