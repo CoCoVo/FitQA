93fba2381394d961b5d81bc482aa8f3693d03adc
olion500 <olion500@daum.net>
2022-04-13 18:59:17 +0900

    Feat : Add feedback registration flow
    You can now add feedback and feedback updated immediately.

--- fitqa_flutter/lib/src/presentation/screens/screen_feedback_request.dart
+++ fitqa_flutter/lib/src/presentation/screens/screen_feedback_request.dart
@@ -1,3 +1,4 @@
+import 'package:fitqa/src/application/feedback/feedback_list.dart';
 import 'package:fitqa/src/presentation/widgets/common/bullet_point_text.dart';
 import 'package:fitqa/src/presentation/widgets/common/fitqa_textfield.dart';
 import 'package:fitqa/src/presentation/widgets/common/labeled_checkbox.dart';
@@ -6,20 +7,19 @@ import 'package:fitqa/src/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ScreenFeedbackRequest extends ConsumerStatefulWidget {
-  const ScreenFeedbackRequest({Key? key}) : super(key: key);
+class ScreenFeedbackRequest extends ConsumerWidget {
+  ScreenFeedbackRequest({Key? key}) : super(key: key);
+
+  final _titleProvider = StateProvider<String>((ref) => "");
+  final _contentProvider = StateProvider<String>((ref) => "");
+  final _lockedProvider = StateProvider<bool>((ref) => false);
 
   @override
-  _ScreenFeedbackRequestState createState() => _ScreenFeedbackRequestState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final titleController = ref.watch(_titleProvider.notifier);
+    final contentController = ref.watch(_contentProvider.notifier);
+    final lockedController = ref.watch(_lockedProvider.notifier);
 
-class _ScreenFeedbackRequestState extends ConsumerState<ScreenFeedbackRequest> {
-  final titleController = TextEditingController();
-  final contentController = TextEditingController();
-  bool locked = false;
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: FColors.white,
         body: ListView(
@@ -53,7 +53,7 @@ class _ScreenFeedbackRequestState extends ConsumerState<ScreenFeedbackRequest> {
                       height: 20,
                     ),
                     FTextField(
-                      controller: titleController,
+                      onChanged: (val) => titleController.state = val,
                       labelText: "제목",
                       hintText: "예시) 벤치할 때 팔꿈치가 아픕니다",
                     ),
@@ -61,7 +61,7 @@ class _ScreenFeedbackRequestState extends ConsumerState<ScreenFeedbackRequest> {
                       height: 16,
                     ),
                     FTextField(
-                      controller: contentController,
+                      onChanged: (val) => contentController.state = val,
                       labelText: "내용",
                       maxLines: 10,
                       hintText:
@@ -71,22 +71,23 @@ class _ScreenFeedbackRequestState extends ConsumerState<ScreenFeedbackRequest> {
                       height: 12,
                     ),
                     LabeledCheckbox(
-                        value: locked,
-                        label: "비공개로 올리기",
-                        onChanged: (v) => {locked = v}),
+                      label: "비공개로 올리기",
+                      onChanged: (val) => {lockedController.state = val},
+                      value: lockedController.state,
+                    ),
                   ]),
             ),
             Align(
               alignment: FractionalOffset.bottomCenter,
               child: InkWell(
                 onTap: () {
-                  // _registerFeedback();
-                  Navigator.of(context).pop();
+                  _registerFeedback(ref);
+                  Navigator.pop(context);
                 },
                 child: Container(
                   height: 70,
                   color: FColors.blue,
-                  child: Center(
+                  child: const Center(
                       child: Text(
                     "결제하기",
                     style: TextStyle(
@@ -101,24 +102,11 @@ class _ScreenFeedbackRequestState extends ConsumerState<ScreenFeedbackRequest> {
         ));
   }
 
-  // void _registerFeedback() {
-  //   final title = titleController.text;
-  //   final content = contentController.text;
-  //   ref.read(registerFeedbackProvider(RegisterFeedback(
-  //       ownerId: "2",
-  //       trainerId: "123",
-  //       interestArea: "LOWER",
-  //       price: 100000,
-  //       title: title,
-  //       content: content,
-  //       locked: locked)));
-  //   ref.refresh(feedbackListProvider);
-  // }
-
-  @override
-  void dispose() {
-    titleController.dispose();
-    contentController.dispose();
-    super.dispose();
+  void _registerFeedback(WidgetRef ref) {
+    final title = ref.watch(_titleProvider);
+    final content = ref.watch(_contentProvider);
+    final locked = ref.watch(_lockedProvider);
+    final feedbackController = ref.watch(feedbackListProvider.notifier);
+    feedbackController.registerFeedback(title, content, locked);
   }
 }
