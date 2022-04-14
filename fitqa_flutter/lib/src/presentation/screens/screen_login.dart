2ffedc19303e5fe99da0d7f4e03812fa7a24c2cb
olion500 <olion500@daum.net>
2022-04-15 00:23:40 +0900

    Chore: Add webview dependency
    Webview is used to get oauth2 token.

--- fitqa_flutter/lib/src/presentation/screens/screen_login.dart
+++ fitqa_flutter/lib/src/presentation/screens/screen_login.dart
@@ -1,3 +1,4 @@
+import 'package:fitqa/src/presentation/widgets/common/social_login_button.dart';
 import 'package:fitqa/src/theme/color.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,9 @@ class ScreenLogin extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisSize: MainAxisSize.min,
               children: [
-                SocialButton(
+                SocialLoginButton(
+                    onTap: () => _launchURL(
+                        "http://localhost:8080/oauth2/authorization/kakao"),
                     title: "Login with Google",
                     color: FColors.socialGoogleColor,
                     icon: Icons.comment)
@@ -26,49 +29,8 @@ class ScreenLogin extends StatelessWidget {
       ),
     );
   }
-}
 
-class SocialButton extends StatelessWidget {
-  SocialButton(
-      {Key? key,
-      required this.title,
-      required this.color,
-      required this.icon,
-      this.onTap})
-      : super(key: key);
-
-  String title;
-  Color color;
-  IconData icon;
-  Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => onTap!(),
-      child: Ink(
-        height: 50,
-        decoration: BoxDecoration(
-            color: color,
-            borderRadius: const BorderRadius.all(Radius.circular(10))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(title,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold)),
-          ],
-        ),
-      ),
-    );
+  void _launchURL(String provider) async {
+    final oauthURL = "http://localhost:8080/oauth2/authorization/$provider";
   }
 }
