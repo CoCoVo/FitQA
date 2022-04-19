a2306f4924ca85249d878a4f3944df786c19f1f0
olion500 <olion500@daum.net>
2022-04-20 02:57:56 +0900

    Feat: Add trainer filtering

--- fitqa_flutter/lib/src/presentation/widgets/trainer/list/trainer_list.dart
+++ fitqa_flutter/lib/src/presentation/widgets/trainer/list/trainer_list.dart
@@ -1,8 +1,10 @@
+import 'package:fitqa/src/application/trainer/trainer_list.dart';
 import 'package:fitqa/src/domain/entities/trainer/trainer/trainer.dart';
-import 'package:fitqa/src/presentation/controller/screen_trainer_controller.dart';
 import 'package:fitqa/src/presentation/widgets/trainer/list/trainer_card_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../common/fitqa_workout_area_filter.dart';
 
 class TrainerList extends ConsumerWidget {
   TrainerList({
@@ -15,41 +17,57 @@ class TrainerList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final trainers = ref.watch(trainerControllerProvider);
+    final trainers = ref.watch(trainerListProvider);
+    final trainerFilters = ref.watch(trainerFilterProvider);
+
+    final trainerFilterController = ref.watch(trainerFilterProvider.notifier);
 
     return Column(mainAxisSize: MainAxisSize.min, children: [
-      // SizedBox(
-      //   height: FDimen.defaultMultiSelectChipSize,
-      //   child: SingleChildScrollView(
-      //       scrollDirection: Axis.horizontal,
-      //       child: MultiSelectChip(
-      //           WorkOutArea.values
-      //               .where((element) => element != WorkOutArea.none)
-      //               .map((e) => e.toStringType())
-      //               .toList(),
-      //           spacing: 8,
-      //           onSelected: (selectedList) {})),
-      // ),
-      trainers.when(
-          data: (trainers) {
-            return Expanded(
-                child: ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              scrollDirection: Axis.vertical,
-              itemCount: trainers.length,
-              itemBuilder: (context, int index) => TrainerCardView(
-                trainer: trainers[index],
-                onTrainerTap: onTrainerTap,
-                onButtonTap: onButtonTap,
-              ),
-            ));
-          },
-          error: (error, e) => Center(
-                child: Text(error.toString()),
-              ),
-          loading: () => const Center(
-                child: CircularProgressIndicator(),
-              )),
+      FitqaWorkoutAreaFilter(
+        selected: trainerFilters,
+        onSelected: (area, selected) {
+          trainerFilterController.update(area, selected);
+        },
+      ),
+      trainers.maybeWhen(
+        success: (trainers) => Expanded(
+            child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          scrollDirection: Axis.vertical,
+          itemCount: trainers.length,
+          itemBuilder: (context, int index) => TrainerCardView(
+            trainer: trainers[index],
+            onTrainerTap: onTrainerTap,
+            onButtonTap: onButtonTap,
+          ),
+        )),
+        error: (err) => Center(
+          child: Text(err.toString()),
+        ),
+        orElse: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      // trainers.when(
+      //     data: (trainers) {
+      //       return Expanded(
+      //           child: ListView.builder(
+      //         padding: const EdgeInsets.symmetric(vertical: 8),
+      //         scrollDirection: Axis.vertical,
+      //         itemCount: trainers.length,
+      //         itemBuilder: (context, int index) => TrainerCardView(
+      //           trainer: trainers[index],
+      //           onTrainerTap: onTrainerTap,
+      //           onButtonTap: onButtonTap,
+      //         ),
+      //       ));
+      //     },
+      //     error: (error, e) => Center(
+      //           child: Text(error.toString()),
+      //         ),
+      //     loading: () => const Center(
+      //           child: CircularProgressIndicator(),
+      //         )),
     ]);
   }
 }
