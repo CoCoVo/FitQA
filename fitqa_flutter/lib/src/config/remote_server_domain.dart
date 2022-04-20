0a41ac2dc73832594efb92f35a27216045ad7a57
Ho2eny <heoin122@gmail.com>
2022-04-21 01:25:23 +0900

    Change remote server url

--- fitqa_flutter/lib/src/config/remote_server_domain.dart
+++ fitqa_flutter/lib/src/config/remote_server_domain.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class RemoteServerDomain {
   static const String localhost = (kIsWeb) ? '127.0.0.1' : '10.0.2.2';
   static const String remoteHost =
-      "ec2-15-164-241-17.ap-northeast-2.compute.amazonaws.com";
+      "ec2-13-125-176-194.ap-northeast-2.compute.amazonaws.com";
 
   static const String host = remoteHost;
   static const String fitqaApiUrl = 'http://$host:8080/api/v1';
